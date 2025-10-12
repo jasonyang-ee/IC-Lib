8cfe537b97dcbf0069021855ff7e93e877195410
Jason Yang <git@jasony.org>
2025-10-11 20:04:46 -0700

    chore: default msg update

--- client/src/pages/Library.jsx
+++ client/src/pages/Library.jsx
@@ -751,7 +751,7 @@ const Library = () => {
               <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
               <input
                 type="text"
-                placeholder="Part number, description..."
+                placeholder="Full data search ..."
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
                 className="w-full pl-10 pr-3 py-2 border border-gray-300 dark:border-[#444444] rounded-md focus:outline-none focus:ring-2 focus:ring-primary-500 bg-white dark:bg-[#333333] dark:text-gray-100"
